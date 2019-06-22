73fe8eb97e5bb65e50ff9d311313438e8a34cd2a
xiaotaoqi <1328611928@qq.com>
2019-06-22 23:37:13 +0800

    Initial commit

---
+++ src/com/sqq/test/Test.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (C), 2015-2019, XXX有限公司
+ * FileName: Test
+ * Author:   xiaotaoqi
+ * Date:     2019-06-22 11:26
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.sqq.test;
+
+
+import java.util.ArrayList;
+
+public class Test {
+    //prsf
+    private static final int NUM = 100;
+
+    //psvm
+    public static void main(String[] args) {
+        //sout
+        System.out.println();
+        //soutp
+        System.out.println("args = [" + args + "]");
+        //soutm
+        System.out.println("Test.main");
+        int num1;
+        int num2;
+        //soutv---就近原则
+
+        //num1.sout
+        //iter
+        for (String arg : args) { }
+        //list.for
+        ArrayList list = new ArrayList();
+        for (Object o : list) {
+        }
+        //list.fori
+        for (int i = 0; i < list.size(); i++) { }
+        //list.forr
+        for (int i = list.size() - 1; i >= 0; i--) { }
+        //ifn
+        if (list == null) { }
+
+
+    }
+        }
+
+
+
+
